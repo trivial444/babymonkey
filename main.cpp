3fb601ffa3ac83749ce514e5216ccce662979a68
admin <admin@localhost.localdomain>
2025-10-18 00:39:05 +0700

    Добавлен файл в индекс

--- main.cpp
+++ main.cpp
@@ -3,8 +3,9 @@
 #define SIZE 1024
 int main(int argc, char** argv) {
  char* str = (char*)malloc(sizeof(char) * SIZE);
- printf("Введите своё имя:\n");
- scanf("%s", str);
+ puts("Привет");
+ puts(str);
+ str = gets(str);
  printf("Привет, %s", str);
  return 0;
 }
